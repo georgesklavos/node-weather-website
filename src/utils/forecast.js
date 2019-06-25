6726cb2c92e2e516c4944231b2dced7e78767c96
george sklavos <laos_2111@yahoo.gr>
2019-06-25 20:55:03 +0300

    Add new text to the weather result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,9 @@ const forecast = (latitude, longitude, callback) => {
         undefined,
         `${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${
           body.currently.precipProbability
-        }% chance of rain`
+        }% chance of rain.The temprature today will be between ${body.daily.data[0].temperatureLow} and ${
+          body.daily.data[0].temperatureHigh
+        } degrees`
       );
     }
   });
